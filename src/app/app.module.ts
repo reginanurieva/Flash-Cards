da0038d8d026edbc4fb6fbfcc91eb69c678d93f3
David Mortkowitz <dmortkowitz@gmail.com>
2018-11-07 14:06:40 -0800

    Add router file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ModuleWithProviders }  from '@angular/core';
+import { FormsModule }  from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
 import { masterFirebaseConfig } from './apikeys';
 import { AngularFireModule } from 'angularfire2';
@@ -9,7 +11,7 @@ import { AppComponent } from './app.component';
 import { SettingsComponent } from './settings/settings.component';
 import { TimerComponent } from './timer/timer.component';
 import { FlashCardsComponent } from './flash-cards/flash-cards.component';
-import { FormsModule }  from '@angular/forms';
+
 import * as $ from 'jquery';
 import { QuizComponent } from './quiz/quiz.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -40,10 +42,10 @@ export const firebaseConfig = {
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     routing,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
-    routing
+    AngularFireDatabaseModule
     // FlipModule,
 
   ],
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,9 +4,20 @@ import { FlashCardsComponent } from './flash-cards/flash-cards.component'
 import { QuizComponent } from './quiz/quiz.component';
 import { SettingsComponent } from './settings/settings.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 
 const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent
+  },
+  {
+    path: 'quiz',
+    component: QuizComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
